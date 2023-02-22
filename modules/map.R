6c9a8d5f034680eb6e70a4401cb7f0e628ba5495
Vanessa Bremerich <vanessa.bremerich@igb-berlin.de>
2023-02-22 17:27:04 +0100

    Replace ESRI base layer with Sentinel-2 cloudless base layer in map module
    The ESRI layer seems to require registration with an ESRI dev account, so let's use the free
    sentinel layer by EOX IT that is also used in GeoNode

--- modules/map.R
+++ modules/map.R
@@ -5,11 +5,11 @@ library(leaflet)
 library(leaflet.extras)
 
 # Module UI function
-mapOutput <- function(id, label = 'maprecords') {
+mapOutput <- function(id, label = "maprecords") {
   ns <- NS(id)
   tagList(
     # map
-    leafletOutput(ns('map'), height=1000)
+    leafletOutput(ns("map"), height = 1000)
   )
 }
 
@@ -17,18 +17,29 @@ mapOutput <- function(id, label = 'maprecords') {
 # columns: ids,  decimalLongitude and decimalLatitude. This data frame comes from
 # the module that upload a CSV file
 #
-mapServer <- function(id, records){
+mapServer <- function(id, records) {
   moduleServer(
     id,
     function(input, output, session) {
 
+      # attribution for Sentinel-2 cloudless 2016 base map
+      s2mapsAttribution <- paste0(
+        '<a xmlns:dct="http://purl.org/dc/terms/"',
+        'href="https://s2maps.eu" property="dct:title">Sentinel-2 cloudless - ',
+        'https://s2maps.eu</a> by <a xmlns:cc="http://creativecommons.org/ns#"',
+        'href="https://eox.at" property="cc:attributionName" rel="cc:attributionURL">',
+        "EOX IT Services GmbH</a> (Contains modified Copernicus Sentinel data 2016 &amp; 2017)"
+      )
+
       # base map
       output$map <- renderLeaflet({
         leaflet() %>%
-          addProviderTiles(providers$Esri.WorldImagery)%>%
-          setView(0,10,2.5)%>%
-          addScaleBar(position = c("bottomleft"),
-                      options = scaleBarOptions(imperial = F))
+          addTiles("https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless_3857/default/g/{z}/{y}/{x}.jpg", s2mapsAttribution) %>%
+          setView(0, 10, 2.5) %>%
+          addScaleBar(
+            position = c("bottomleft"),
+            options = scaleBarOptions(imperial = F)
+          )
       })
 
       # # If no CSV is uploaded, don't do anything, otherwise create a reactive
@@ -44,23 +55,24 @@ mapServer <- function(id, records){
           lapply(htmltools::HTML)
         # points
         leafletProxy("map", data = point()) %>%
-            addCircleMarkers(data = point(),
-              lng = ~decimalLongitude,
-              lat = ~decimalLatitude,
-              fillColor =  "blue",
-              fillOpacity = 0.7, color="black",
-              radius=5,
-              stroke=T,
-              weight = 0.3,
-              label = labeltext,
-              labelOptions = labelOptions(
-                style = list("font-weight" = "normal", padding = "3px 8px"),
-                textsize = "13px",
-                direction = "bottom",
-                opacity = 0.9)
+          addCircleMarkers(
+            data = point(),
+            lng = ~decimalLongitude,
+            lat = ~decimalLatitude,
+            fillColor = "blue",
+            fillOpacity = 0.7, color = "black",
+            radius = 5,
+            stroke = T,
+            weight = 0.3,
+            label = labeltext,
+            labelOptions = labelOptions(
+              style = list("font-weight" = "normal", padding = "3px 8px"),
+              textsize = "13px",
+              direction = "bottom",
+              opacity = 0.9
             )
+          )
       })
-
     }
   )
 }
