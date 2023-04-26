5206857761439c0431e7328e3dbc9495480de7fe
Vanessa Bremerich <vanessa.bremerich@igb-berlin.de>
2023-04-26 16:18:19 +0200

    table stream_nodes temporarily not available

--- R/utils_db_tables.R
+++ R/utils_db_tables.R
@@ -18,15 +18,15 @@ stream_order_tbl <- tbl(pool, in_schema("hydro", "stream_order"))
 # stream_segments
 #stream_segments_tbl <- tbl(pool, in_schema("hydro", "stream_segments"))
 
-# stream_nodes
-stream_nodes_tbl <- tbl(pool, in_schema("hydro", "stream_nodes"))
+# stream_nodes (temporarily not available)
+# stream_nodes_tbl <- tbl(pool, in_schema("hydro", "stream_nodes"))
 
 # sub_catchments
 sub_catchments_tbl <- tbl(pool, in_schema("hydro", "sub_catchments"))
 
 ## environmental variables tables
 # stats_topo
-#stats_topo_tbl <- tbl(pool, in_schema("hydro", "stats_topo"))
+stats_topo_tbl <- tbl(pool, in_schema("hydro", "stats_topo"))
 
 # stats_climate
 stats_climate_tbl <- tbl(pool, in_schema("hydro", "stats_climate"))
