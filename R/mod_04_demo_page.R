21f6a3bc58041ce2df90fc955ddf13514ba1f4ac
afrogri37 <afroditi.grigoropoulou@igb-berlin.de>
2023-04-27 11:24:29 +0200

    replaced html with md

--- R/mod_04_demo_page.R
+++ R/mod_04_demo_page.R
@@ -1,5 +1,6 @@
 # This module configures the content of the GeoFRESH app demo page
 # demo page module UI function
+
 demoPageUI <- function(id, label = "demo_page") {
   ns <- NS(id)
 
@@ -10,9 +11,10 @@ demoPageUI <- function(id, label = "demo_page") {
         fluidPage(
       # titlePanel("Included Content"),
       mainPanel(
-        includeHTML("../static/geofresh/tutorial.html"), width = 100000
+        includeMarkdown("www/tutorial.md"), width = 100000
       )
     )
+
 
     )
 
@@ -28,7 +30,4 @@ demoPageUI <- function(id, label = "demo_page") {
 demoPageServer <- function(input, output, session) {
 
       # TODO
-  # output$markdown <- renderUI({
-  #   HTML(markdown::markdownToHTML(knit('./www/tutorial.Rmd', quiet = TRUE)))
-  # })
     }
