db8e6d42374eb40af2d73febb1873c63640fbbb1
bremerich <bremerich@igb-berlin.de>
2023-09-07 15:36:58 +0200

    Add button label and file name parameters to download module functions

--- R/mod_download_csv.R
+++ R/mod_download_csv.R
@@ -2,28 +2,27 @@
 
 # Module UI function
 
-downloadDataUI <- function(id) {
-    ns <- NS(id)
-    tagList(
-      downloadButton(ns("downloadData"), "Download", icon = NULL)
-    )
-  }
+downloadDataUI <- function(id, label = "Download") {
+  ns <- NS(id)
+  tagList(
+    downloadButton(ns("downloadData"), label, icon = NULL)
+  )
+}
 
 # Module server function
 
-downloadDataServer <- function(id, data_download) {
-    moduleServer(
-      id,
-      function(input, output, session) {
-        output$downloadData <- downloadHandler(
-          filename = function() {
-            paste("geofresh-", Sys.Date(), ".csv", sep="")
-          },
-          content = function(file) {
-            write.csv(data_download, row.names = FALSE, file)
+downloadDataServer <- function(id, data, file_name = "") {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      output$downloadData <- downloadHandler(
+        filename = function() {
+          paste("geofresh-", Sys.Date(), file_name, ".csv", sep = "")
+        },
+        content = function(file) {
+          write.csv(data, row.names = FALSE, file)
         }
       )
     }
   )
 }
-
