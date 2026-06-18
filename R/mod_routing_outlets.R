ccfb7d98b3d14e2b227c68864d4b8c260a98064f
Merret B <merret.buurman@igb-berlin.de>
2026-06-18 22:37:39 +0200

    Routing: Downstream: If any points were already snapped, only use snapped coordinates or manually moved.

--- R/mod_routing_outlets.R
+++ R/mod_routing_outlets.R
@@ -51,17 +51,24 @@ routingServer <- function(id, points_db, paths_to_outlet) {
       df <- points_db()
 
       # NOTE: If the points are snapped, we should use their subc_id, not just their coordinates! (faster!)
-      #has_snapped <- all(c("latitude_snap", "longitude_snap") %in% names(df)) &&
-      #  any(is.finite(df$latitude_snap) & is.finite(df$longitude_snap))
-      #df <- isolate(point$snap_points())
+      # Check if data frame df contains both columns "latitude_snap" and "longitude_snap"
+      has_cols <- all(c("latitude_snap", "longitude_snap") %in% names(df))
+      # Check if at least one row has finite values in both columns.
+      has_at_least_one_finite_row <- any(is.finite(df$latitude_snap) & is.finite(df$longitude_snap))
+      # So:
+      has_snapped <- has_cols && has_at_least_one_finite_row
 
       showNotification(paste0("Requesting path to sea for ", nrow(df), " points: This may take a while, please be patient."))
       #showNotification("Paths will be shown only after you zoom or pan the map.")
       for (i in seq_len(nrow(df))) {
         #showNotification(paste("Now preparing promise, treating row:", i, "..."))
         promise <- future_promise({
-          # TODO IMPORTANT: Which columns to use?
-          fetch_from_pygeoapi_outlet(lon=df$longitude[i], lat=df$latitude[i])
+          if (has_snapped) {
+            # TODO: Handle gracefully if a point could not be snapped and containes NULL (or so)!
+            fetch_from_pygeoapi_outlet(lon=df$longitude_snap[i], lat=df$latitude_snap[i])
+          } else {
+            fetch_from_pygeoapi_outlet(lon=df$longitude[i], lat=df$latitude[i])
+          }
           #fetch_from_pygeoapi_outlet(subc_id=df$subc_id[i])
         }, seed = TRUE)
         #showNotification(paste("Prepared promise no:", i, ", coordinates: ", df$longitude[i], df$latitude[i]))
